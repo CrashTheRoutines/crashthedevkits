f9e8fdaacf2b120aa79e5975ead4478a5f9b8d60
Adam Lean <adam.lean.92@gmail.com>
2025-07-03 13:09:04 +0800

    Update bot.py

--- python/python-telegram-bot/bot.py
+++ python/python-telegram-bot/bot.py
@@ -15,3 +15,48 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
+
+
+# Reply buttom
+
+from telegram import ReplyKeyboardMarkup
+
+keyboard = [
+    ["Кнопка 1", "Кнопка 2"],
+    ["Кнопка 3"]
+]
+
+reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+
+await update.message.reply_text("Выберите кнопку:", reply_markup=reply_markup)
+
+# Inline buttom
+
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton
+
+keyboard = [
+    [
+        InlineKeyboardButton("Нажми меня", callback_data="my_callback"),
+        InlineKeyboardButton("Сайт", url="https://example.com")
+    ]
+]
+
+reply_markup = InlineKeyboardMarkup(keyboard)
+
+await update.message.reply_text("Нажмите кнопку:", reply_markup=reply_markup)
+
+from telegram.ext import CallbackQueryHandler
+
+async def handle_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    await query.answer()
+    await query.edit_message_text(f"Вы нажали: {query.data}")
+
+# в main.py
+app.add_handler(CallbackQueryHandler(handle_callback))
+
+# Delete keyboard
+
+from telegram import ReplyKeyboardRemove
+
+await update.message.reply_text("Клавиатура скрыта", reply_markup=ReplyKeyboardRemove())
